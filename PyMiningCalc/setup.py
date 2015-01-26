7ca774e9c1c32ba687f26bcfa43471a14ef315b2
Jason Crockett <deddokatana@gmail.com>
2015-01-26 07:12:06 +0000

    added Setup

---
+++ PyMiningCalc/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyMiningCalc',
+    version='1.0-Alpha',
+    packages=['PyMiningCalc'],
+    url='https://sites.google.com/site/working4coins/calculator, https://github.com/deddokatana/PyMiningCalc',
+    license='GPL3',
+    author='Twitter : @deddokatana  - @WorkingForCoins',
+    author_email='deddokatana@gmail.com',
+    description='Python3 Output Calculator for Bitcoin/Litecoin Based Currencies'
+)
